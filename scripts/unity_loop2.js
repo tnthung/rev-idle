28fb65ef13a855426701fb41ab4767a9a6617de6
TNThung <hung11112002@gmail.com>
2026-09-09 06:12:56 +0800

    [Chg] adjust window height in main initialization for better layout

--- scripts/unity_loop2.js
+++ scripts/unity_loop2.js
@@ -28,7 +28,7 @@ let executionConfig = {};
 export default async function main() {
   // local initialization
   if (!initialized) {
-    rev.resize(1270, 600);
+    rev.resize(1270, 470);
     console.clear();
     Action.dismissLoop();
     initialized = true;
