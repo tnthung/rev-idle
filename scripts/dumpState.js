fbad7cb1a26e989631066fd4c9f426e6850f6816
TNThung <hung11112002@gmail.com>
2026-09-10 16:30:43 +0800

    [Chg] update dumpState function to write to __dump.json and ensure rev.stop() is called

--- scripts/dumpState.js
+++ scripts/dumpState.js
@@ -2,10 +2,11 @@ import { States } from "./lib/states";
 
 
 export default async function() {
-  rev.write_file("__zodiac.json", JSON.stringify({
+  rev.write_file("__dump.json", JSON.stringify({
     planetInventory: await States.planetZodiacInventory(),
     unityInventory: await States.unityZodiacInventory(),
     sacrificeState: await States.sacrificeState(),
     attackRelics: await States.attackRelics(),
   }, null, 2));
+  rev.stop();
 }
